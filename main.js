2fd2859f5cc865e0df29a2486527aac7bec4df32
StephanPumpernik <stephan.pumpernik@gmx.net>
2025-05-15 18:02:38 +0200

    wettericons für die nächsten 24h eingefügt

--- main.js
+++ main.js
@@ -55,6 +55,11 @@ async function showForecast(latlng) {
     </ul>
     `; 
 
+    //Wettericons für die nächste 24h in 3h Schritten
+    for (let i=0; i <=24; i+=3) {
+        let symbol = jsondata.properties.timeseries[i].data.next_1_hours.summary.symbol_code;
+        console.log(symbol);
+    }
     
     L.popup([
         latlng.lat, latlng.lng
